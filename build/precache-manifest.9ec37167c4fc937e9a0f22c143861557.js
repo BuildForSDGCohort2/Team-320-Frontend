7deb84e586bc656926ebce302242b514e42add31
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2020-08-28 20:06:14 +0100

    Add files via upload.
    Basic Landing Page Design

---
+++ build/precache-manifest.9ec37167c4fc937e9a0f22c143861557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d887c49745450fe9f4c2b0b5dd33580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f56029f0fee6c86995f",
+    "url": "/static/css/main.a6d41c03.chunk.css"
+  },
+  {
+    "revision": "570b02f3e35cdb40b97e",
+    "url": "/static/js/2.f5e95950.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5e95950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f56029f0fee6c86995f",
+    "url": "/static/js/main.59430ee6.chunk.js"
+  },
+  {
+    "revision": "2f3fb58a55316722e357",
+    "url": "/static/js/runtime-main.fb102db6.js"
+  }
+]);
